35804645c747cab5fe66de90cd50df322cba605b
justinh225 <jah00031@mix.wvu.edu>
2024-09-15 12:42:20 -0400

    MenuButton now works and search seems to work

--- main.py
+++ main.py
@@ -1,49 +1,69 @@
 import tkinter as tk
 import tkinter.font as tk_font
 
-global search_type
-
 background_color = "#B3FF00"
 text_color = "#0035FF"
 store_name = "20 For 20 Bookstore"
 subtitle = "A bookstore for readers"
 manager = "Justin Heimes"
 book_catalog = {
-    "Kids": [ {
+    "Kids": {
         "No, David": "David Shannon",
         "David Goes to School": "David Shannon",
         "David Gets in Trouble": "David Shannon",
         "It's Christmas, David!": "David Shannon",
         "Grow Up, David": "David Shannon"
-    }],
-    "Horror": [{
+    },
+    "Horror": {
         "It": "Stephen King",
         "The Shining": "Stephen King",
         "Cabin at the End of the World": "Paul G. Tremblay",
         "The Call of Cthulhu": "H.P. Lovecraft",
         "The Cats of Ulthar": "H.P. Lovecraft"
-    }],
-    "Dystopian": [{
+    },
+    "Dystopian": {
         "The Hunger Games": "Suzanne Collins",
         "Catching Fire": "Suzanne Collins",
         "Mockingjay": "Suzanne Collins",
         "The Ballad of Songbirds and Snakes": "Suzanne Collins",
         "1984": "George Orwell"
-    }],
-    "Classical": [{ 
+    },
+    "Classical": { 
         "Divine Comedy": "Dante Alighieri",
         "Meditations": "Seneca",
         "Crime and Punishment": "Fyodor Dostoevsky",
         "Animal Farm": "George Orwell",
         "The Catcher in the Rye": "J.D. Salinger" 
-    }]
+    }
 }
+
+global genre_search
+global author_search
+global title_search
 
 # Gets the value from a search bar and updates mainpage.
 # If the search bar is empty, displays all items
 def search_mainpage():
-    search_string = search_box.get()
-
+    search_content = search_box.get("1.0", tk.END)
+    search_content = search_content.strip()
+    if(genre_search.get()):
+        for key in book_catalog:
+            if(key == search_content):
+                label = tk.Label(text="Genre")
+                label.pack(side="top", anchor="w")
+    if(author_search.get()):
+        for genre in book_catalog:
+            for author in book_catalog[genre]:
+                if(author == search_content):
+                    label = tk.Label(text="Author")
+                    label.pack(side="top", anchor="w")
+    if(title_search.get()):
+        for genre in book_catalog:
+            for author in book_catalog[genre]:
+                for title in book_catalog[author]:
+                    if(title == search_content):
+                        label = tk.Label(text="Title")
+                        label.pack(side="top", anchor="w")
 
 #Setup Window
 main_window = tk.Tk()
@@ -66,18 +86,24 @@ subtitle_label.pack(side="top")
 heading_label.pack(side="top")
 
 # Add Search Bar
-search_button = tk.Button(text="Search", height=1)
+search_button = tk.Button(text="Search", height=1, command=search_mainpage)
 search_button.pack(side="left", anchor="w", padx=15)
 search_box = tk.Text(height=1)
 search_box.pack(side="left", anchor="w", padx=15)
-search_menu = tk.Menu()
 
-search_menu.add_radiobutton(label="Genre")
-search_menu.add_radiobutton(label="Author")
-search_menu.add_radiobutton(label="Title")
-search_type = tk.Menubutton(text="Search Type", relief="raised", menu=search_menu)
+# Setup MenuButton
+search_type = tk.Menubutton(master=main_window, text="Search Type", relief="raised")
+search_type.menu = tk.Menu(search_type)
+search_type["menu"] = search_type.menu
+
+# Add Options and pack
+genre_search = tk.BooleanVar()
+author_search = tk.BooleanVar()
+title_search = tk.BooleanVar()
+search_type.menu.add_checkbutton(label="Genre", variable=genre_search)
+search_type.menu.add_checkbutton(label="Author", variable=author_search)
+search_type.menu.add_checkbutton(label="Title", variable=title_search)
 search_type.pack(side="left", anchor="w", padx=15)
 
+# MainLoop
 main_window.mainloop()
-
-
